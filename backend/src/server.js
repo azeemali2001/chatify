783dc00f4325b228fa6f325cb498f5b91c88d3a0
Azeem Ali <aliazeem7428@gmail.com>
2025-09-23 21:55:53 +0530

    Ready for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -14,6 +14,17 @@ import messageRoute from "./routes/message.route.js"
 app.use("/api/auth", authRoute);
 app.use("/api/messages", messageRoute);
 
+import path from "path";
+const __dirname = path.resolve();  //use in module JS
+
+if(process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    })
+}
+
 const PORT = process.env.PORT || 3000;
 app.listen(3000, () => {
     console.log("Server is running on ", PORT);
