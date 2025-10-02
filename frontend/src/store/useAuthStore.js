a158985ee503a22a1de8b315e9714c01b0534f07
Azeem Ali <aliazeem7428@gmail.com>
2025-10-02 20:40:26 +0530

    login-logout fronted implemented

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -4,8 +4,9 @@ import toast from "react-hot-toast";
 
 export const useAuthStore = create((set) => ({
     authUser: null,
-    isCheckingAuth: true,
 
+
+    isCheckingAuth: true,
     checkAuth: async () => {
         try {
             const res = await axiosInstance.get("/auth/check");
@@ -19,7 +20,6 @@ export const useAuthStore = create((set) => ({
     },
 
     isSigningUp: false,
-
     signup: async (data) => {
         set({ isSigningUp: true });
         try {
@@ -31,6 +31,32 @@ export const useAuthStore = create((set) => ({
         } finally {
             set({ isSigningUp: false })
         }
-    }
+    },
+
+    isLoggingIn: false,
+    login: async (data) => {
+        set({ isLoggingIn: true });
+        try {
+            const res = await axiosInstance.post("/auth/login", data);
+            set({ authUser: res.data });
+            toast.success("Loged in Successfull");
+        } catch (error) {
+            toast.error(error.response.data.message);
+        } finally {
+            set({ isLoggingIn: false })
+        }
+    },
+
+    logout: async () => {
+        try {
+            await axiosInstance.post("/auth/logout");
+            set({ authUser: null });
+            toast.success("Logged out successfully");
+        } catch (error) {
+            toast.error("Error in Logging out");
+            console.log("Error in loggin out", error);
+        }
+    },
+
 
 }))
